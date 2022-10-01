d47efcb0a4d268677cc453d78a45f43ea5bef471
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2022-10-01 14:52:39 +0530

    menubar_Bug_Fixed_And_Updated

--- MidBody.js
+++ MidBody.js
@@ -7,13 +7,29 @@ document.querySelector("#menu").addEventListener("click",fun);
 let count=+0;
 
 function fun(){
+    document.querySelector("#menu>div").innerHTML=null;
+
+    document.querySelector("#cross").hidden=false;
+
+
+    document.querySelector("#slide_container").innerHTML=null;
     count++;
 
     if(count%(+2)==+0){
         location.reload();
+        document.querySelector("#slide_container").innerHTML=null;
     }
+    else{
     let div= document.createElement("div");
         div.setAttribute("id","append");
+
+    let div0= document.createElement("div");
+        div0.setAttribute("id","barPeraDiv");
+    let p0=document.createElement("P");
+        p0.innerText="Discounts";
+
+    let p00=document.createElement("P");
+        p00.innerText="New Products";
 
     let p1=document.createElement("p");
         p1.innerText="WHAT'S NEW?";
@@ -41,6 +57,19 @@ function fun(){
     let p12=document.createElement("p");
         p12.innerText="LOG IN";
 
-    div.append(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12);
-    document.querySelector("#menu").append(div);
+    div0.append(p0,p00,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12);
+    div.append(div0);
+    document.querySelector("#slide_container").append(div);
+
+    let div2=document.createElement("div");
+        div2.setAttribute("id","barImageContainer");
+
+   let img= document.createElement("img");
+   img.setAttribute("src","https://cdn.shopify.com/s/files/1/0361/8553/8692/files/vit_c_3_2.png?v=1663651282");
+   img.setAttribute("id","barImage")
+
+   div2.append(img)
+   document.querySelector("#append").append(div2);
+    }
 }
+
